9bbc0a35ebacc4985b813f59cb6f8c3d4383d56e
Cleyton12 <44759584+Cleyton12@users.noreply.github.com>
2025-04-12 16:23:36 -0300

    Delete api directory

--- api/index.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  if (req.method === 'POST') {
-    console.log('Dados recebidos:', req.body); // Aqui chegam os dados do TradingView
-    res.status(200).json({ sucesso: true, mensagem: 'Sinal recebido!' });
-  } else {
-    res.status(405).json({ erro: 'Método não permitido' });
-  }
-}
